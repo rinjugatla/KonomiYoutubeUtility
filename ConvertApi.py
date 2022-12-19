2f83cbcd20f4df92122365f9f6233207e7e61cb6
rinjugatla <rin.jugatla@gmail.com>
2022-12-19 18:39:34 +0900

    Markdownファイル出力機能追加

--- ConvertApi.py
+++ ConvertApi.py
@@ -10,11 +10,29 @@ def get_api(url: str) -> str:
 
     return response.json()
 
+def create_header_text() -> str:
+    header = '''# このみさんのお歌リスト（歌枠）
+
+:::info
+- [このみさんのお歌リスト（歌枠）](https://hackmd.io/@spaghetti/Hkvij39ds)からデータを取得しています。
+- 怒られたら消します。
+:::'''
+
+    return header
+
+def save_to_markdown(youtube_lives: YoutubeLives):
+    header = create_header_text()
+    lives_md = youtube_lives.to_markdown('##', '1. ')
+
+    with open('live_md.md', 'w', encoding='utf-8') as f:
+        f.write(header)
+        f.write('\n\n')
+        f.write(lives_md)
+
 api_url = 'https://api.mofucloud.com/archive/videos?_sort=date_published'
 live_data = get_api(api_url)
 if live_data is None:
     exit()
 
 youtube_lives = YoutubeLives(live_data)
-md = youtube_lives.to_markdown('##', '1. ')
-print(md)
+save_to_markdown(youtube_lives)
